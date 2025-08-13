e90169cf3f67f147c68405ee1bc0728507da178c
cynicism <65233110+Cynicism-lab@users.noreply.github.com>
2025-08-13 19:49:27 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,10 @@ set -e
 npm run build # 生成静态文件
 cd docs/.vuepress/dist # 进入生成的文件夹
 
-# deploy to github
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:cynicism-lab/cynicism-lab.github.io.git
-  
-else
-  msg='来自github action的自动部署'
-  githubUrl=https://cynicism-lab:${GITHUB_TOKEN}@github.com/cynicism-lab.github.io.git
-  git config --global user.name "cynicism-lab"
-  git config --global user.email "1711546733@qq.com"
-fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f git@github.com:Cynicism-lab/cynicism-lab/cynicism-lab.github.io.git master:gh-pages # 推送到github
 
 
 cd -
